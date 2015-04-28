43725a936bc1ceac80265b33f16a170d76215b8f
SamCD <samuel.cohen-devries@spsmail.cuny.edu>
2015-04-28 14:31:36 -0400

    Update todoapp.py

--- todoapp.py
+++ todoapp.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
 from flask import request,redirect,url_for
 import re
+import pickle
+import os.path
 app = Flask(__name__)
 
-todo = []
+loaded = False
 err_msg = ''
+
+if os.path.isfile('todolist.pkl'):
+    pkl = open('todolist.pkl','rb')
+    todo = pickle.load(pkl)
+    pkl.close()
+    loaded = True
+else:
+    todo = []
 
 @app.route('/')
 def print_list():
@@ -28,8 +38,19 @@ def submit():
 def clear():
     del todo[:]
     return redirect(url_for('print_list'))
+
+@app.route('/write', methods = ['POST'])
+def write():
+    if loaded:
+        fh = open('todolist.pkl','ab')
+    else:
+        fh = open('todolist.pkl','wb')
+    pickle.dump(todo,fh)
+    fh.close()
+    return redirect(url_for('print_list'))
     
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
